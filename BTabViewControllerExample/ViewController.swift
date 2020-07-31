2e51ab579c1831d907e7fd729305f5e1f6a3df83
Berkay Vurkan <berkayvurkan@yahoo.com>
2020-07-31 21:59:15 +0300

    example project init

---
+++ BTabViewControllerExample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BTabViewControllerExample
+//
+//  Created by Berkay Vurkan on 31.07.2020.
+//  Copyright © 2020 Foo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
